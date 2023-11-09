0b8843d94fb7beb5ce754af7e5dacaf05c6266fa
kanishkasubash <kanishkasubash@gmail.com>
2023-11-10 01:37:28 +0530

    modified:   postcss.config.js 	modified:   src/index.js 	modified:   src/styles/base/_base.scss

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
         'postcss-preset-env': {
             stage: 3, // Use only stable CSS features
             autoprefixer: {
-                browsers: 'last 2 versions', // Specify the target browsers
+                overrideBrowserslist: 'last 2 versions', // Specify the target browsers
             },
         },
     },
